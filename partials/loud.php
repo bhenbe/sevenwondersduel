ed5a06cc665329e95a2568541cfe5338bcc9e6b2
Benoit Henry <benoit.henry@mediakod.com>
2019-10-17 15:21:15 +0200

    responsive png size

--- partials/loud.php
+++ partials/loud.php
@@ -2,13 +2,36 @@
 
     <div class="container clearfix">
 
-        <img src="./assets/img/header-iphone.png" alt="" />    
+        <img sizes="(max-width: 1500px) 100vw, 1500px"
+            srcset="
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_200.png 200w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_361.png 361w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_480.png 480w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_586.png 586w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_676.png 676w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_755.png 755w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_828.png 828w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_903.png 903w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_968.png 968w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_1028.png 1028w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_1082.png 1082w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_1142.png 1142w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_1200.png 1200w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_1259.png 1259w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_1311.png 1311w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_1363.png 1363w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_1413.png 1413w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_1468.png 1468w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_1491.png 1491w,
+            ./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_1500.png 1500w"
+            src="./assets/img/header-iphone/header-iphone_itwo6m_c_scale,w_1500.png"
+            alt=""/>    
 
         <div class="content">
 
             <h1><?php echo $content['body']['loud']['heading']; ?></h1>
-
-            <img class="brand" src="./assets/img/brand.png" alt="7 Wonders Duel"/>
+            
+            <img class="brand" src="./assets/img/brand.png" alt="7Wonders Duel" />
 
             <a href="<?php echo $content['body']['loud']['google-play']; ?>"><img src="./assets/img/google-play.btn.png" alt="Google Play"/></a>
 

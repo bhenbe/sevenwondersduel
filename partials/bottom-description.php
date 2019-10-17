489d2f4aba731c3d1e719398f927e4c6c8cb4257
Benoit Henry <benoit.henry@mediakod.com>
2019-10-17 15:46:10 +0200

    add favicon and manifest

--- partials/bottom-description.php
+++ partials/bottom-description.php
@@ -12,6 +12,31 @@
 
     </div>
 
-    <img class="lazy" data-src="./assets/img/construction.png" alt=""/>
 
+    <img class="lazy"
+    sizes="(max-width: 757px) 100vw, 757px"
+    srcset="
+    ./assets/img/construction/construction_ggn63g_c_scale,w_200.png 200w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_257.png 257w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_305.png 305w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_348.png 348w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_389.png 389w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_425.png 425w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_458.png 458w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_489.png 489w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_518.png 518w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_548.png 548w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_576.png 576w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_603.png 603w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_628.png 628w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_654.png 654w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_679.png 679w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_703.png 703w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_727.png 727w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_750.png 750w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_755.png 755w,
+    ./assets/img/construction/construction_ggn63g_c_scale,w_757.png 757w"
+    src="./assets/img/construction/construction_ggn63g_c_scale,w_757.png"
+    alt="">
+    
 </section>
--- partials/requirements.php
+++ partials/requirements.php
@@ -26,6 +26,16 @@
 
     </div>
 
-    <img class="lazy" data-src="./assets/img/gladiator.png" alt=""/>
+    <img class="lazy"
+    sizes="(max-width: 331px) 100vw, 331px"
+    srcset="
+    ./assets/img/gladiator/gladiator_x3gsmo_c_scale,w_200.png 200w,
+    ./assets/img/gladiator/gladiator_x3gsmo_c_scale,w_238.png 238w,
+    ./assets/img/gladiator/gladiator_x3gsmo_c_scale,w_272.png 272w,
+    ./assets/img/gladiator/gladiator_x3gsmo_c_scale,w_304.png 304w,
+    ./assets/img/gladiator/gladiator_x3gsmo_c_scale,w_328.png 328w,
+    ./assets/img/gladiator/gladiator_x3gsmo_c_scale,w_331.png 331w"
+    src="./assets/img/gladiator/gladiator_x3gsmo_c_scale,w_331.png"
+    alt="">
 
 </section>
